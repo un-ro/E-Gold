eebe04fa57978859f5cdf9a39c9a6757e3726b35
Unero <ramadhanbhg@gmail.com>
2020-12-07 19:31:24 +0700

    Add Item Layout for ListView

--- app/src/main/java/com/unero/e_gold/models/Profile.kt
+++ app/src/main/java/com/unero/e_gold/models/Profile.kt
@@ -1,10 +1,14 @@
 package com.unero.e_gold.models
 
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
+@Entity(tableName = "profile")
 data class Profile(
+    @PrimaryKey(autoGenerate = true) var id: Int,
     private val username: String,
     private val email: String,
     private val image: String
